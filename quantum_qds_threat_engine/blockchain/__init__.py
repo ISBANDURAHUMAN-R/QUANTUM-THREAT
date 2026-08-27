7b91c5e2fa8ae683f8f9c3fc5faff1f5b66b0e03
ISBANDU RAHUMAN R <riyasfamily7@gmail.com>
2026-08-27 21:21:44 +0530

    Delete quantum_qds_threat_engine directory

--- quantum_qds_threat_engine/blockchain/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Blockchain package.
-"""
-from blockchain.qds_oracle_bridge import QDSBlockchainOracleBridge
--- quantum_qds_threat_engine/simulation/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Simulation package for Quantum-Inspired Cyber Threat Detection.
-"""
-
-from simulation.noise_channels import (
-    apply_depolarizing_noise, apply_bit_flip_noise, apply_phase_flip_noise, apply_amplitude_damping
-)
-from simulation.attack_simulator import QuantumAttackSimulator
-from simulation.benchmark_engine import BenchmarkEngine
